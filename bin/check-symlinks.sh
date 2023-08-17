377a7f5397191d48003b00190f0686aaf4a28a98
Jesse Donat <donatj@gmail.com>
2023-08-17 15:30:21 -0500

    Mildly improved output

--- bin/check-symlinks.sh
+++ bin/check-symlinks.sh
@@ -2,12 +2,26 @@
 
 set -euo pipefail
 
-BASEDIR="$(dirname "$0")"/..
+if [ $# -ne 1 ] ; then
+	echo "Usage: $0 <basedir>"
+	exit 1
+fi
+
+BASEDIR="$1"
+if [ ! -d "$BASEDIR" ] ; then
+	echo "BASEDIR $BASEDIR does not exist"
+	exit 1
+fi
+
 FILES=$(find "$BASEDIR" -type l)
 
+ERROR=0
 for F in $FILES; do
 	if [ ! -e "$F" ] ; then
-		echo "broken symlink $F"
-		exit 1
+		LINK=$(readlink "$F")
+		echo "::error file=$F,line=1::Broken symlink to '$LINK'"
+		ERROR=1
 	fi
 done
+
+exit $ERROR
